8b1c65ac03c740d150f757489bcff082b9ff4382
Opiyo Zentone Obunge <windersonrolles@gmail.com>
2019-10-05 11:32:44 +0300

    Second Commit

--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,11 @@
 import datetime
 from django.db import models
 from django.utils import timezone
-# Create your models here.
 
+# Create your models here.
+'''from django.conf.urls import url, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets'''
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -27,3 +30,33 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+# Serializers define the API representation.
+
+
+'''class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+
+# ViewSets define the view behavior.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
+'''
